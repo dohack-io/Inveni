d710e85e161da8622c4e5e5acf0b9b6ac5cabd3c
maucon <maurice.nocon@adesso.de>
2019-09-28 17:00:23 +0200

    Added options menu to actionbar

--- app/src/main/java/de/inveni/MainActivity.java
+++ app/src/main/java/de/inveni/MainActivity.java
@@ -2,7 +2,6 @@ package de.inveni;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
+
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,9 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        System.out.println("HELLO");
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 }
--- app/src/main/java/de/inveni/ui/main/FragmentFound.java
+++ app/src/main/java/de/inveni/ui/main/FragmentFound.java
@@ -1,10 +1,15 @@
 package de.inveni.ui.main;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 import de.inveni.R;
 
@@ -17,12 +22,49 @@ public class FragmentFound extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_found, container, false);
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int pos, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.listview_item, null);
+
+            ImageView imageView = view.findViewById(R.id.imageView3);
+            TextView textView1 = view.findViewById(R.id.textView2);
+            TextView textView2 = view.findViewById(R.id.textView3);
+
+            textView1.setText("mIOJN");
+            textView2.setText("mI23OJN");
+
+            return view;
+        }
+
     }
 
 }
