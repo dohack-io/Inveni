37efb06e6b7e87f2291981a8239e63be0ce269c0
maucon <maurice.nocon@adesso.de>
2019-09-29 09:21:24 +0200

    Added login-activity

--- app/src/main/java/de/inveni/ui/login/LoginViewModel.java
+++ app/src/main/java/de/inveni/ui/login/LoginViewModel.java
@@ -1,15 +1,15 @@
 package de.inveni.ui.login;
 
+import android.util.Patterns;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import android.util.Patterns;
-
+import de.inveni.R;
 import de.inveni.data.LoginRepository;
 import de.inveni.data.Result;
 import de.inveni.data.model.LoggedInUser;
-import de.inveni.R;
 
 public class LoginViewModel extends ViewModel {
 
@@ -29,16 +29,18 @@ public class LoginViewModel extends ViewModel {
         return loginResult;
     }
 
-    public void login(String username, String password) {
+    public boolean login(String username, String password) {
         // can be launched in a separate asynchronous job
         Result<LoggedInUser> result = loginRepository.login(username, password);
 
         if (result instanceof Result.Success) {
             LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
             loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+            return true;
         } else {
             loginResult.setValue(new LoginResult(R.string.login_failed));
         }
+        return false;
     }
 
     public void loginDataChanged(String username, String password) {
